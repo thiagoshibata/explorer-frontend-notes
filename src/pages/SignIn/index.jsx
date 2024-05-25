cc15677776ec454a933ad289853ad6c374b911f4
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-25 10:41:38 -0300

    feat: created page SignIn

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,22 @@
+import { Container, Form, Background } from "./styles"
+import { FiMail, FiLock } from "react-icons/fi"
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rcoket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Faça seu login</h2>
+        <Input icon={FiMail} type="text" placeholder="E-mail" />
+        <Input icon={FiLock} type="password" placeholder="Senha" />
+        <Button title="Entrar" />
+        <a href="#">Criar conta</a>
+      </Form>
+      <Background />
+    </Container>
+  )
+}
