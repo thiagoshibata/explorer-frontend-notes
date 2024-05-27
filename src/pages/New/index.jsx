c83a8748ea755b933f7bef8e344c268430fcc24b
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-27 16:21:41 -0300

    feat: create header on page new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,12 +1,21 @@
-import { Container } from "./styles"
+import { Container, Form } from "./styles"
 
 import { Header } from "../../components/Header"
+import { Input } from "../../components/Input"
 
 export function New() {
   return (
     <Container>
       <Header />
-      <h1>Criar nota</h1>
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+          <Input placeholder="Título" />
+        </Form>
+      </main>
     </Container>
   )
 }
