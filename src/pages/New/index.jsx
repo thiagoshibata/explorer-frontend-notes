559bca10c16c4afbfb1ef6717adbb2f6356e12d9
thiago-shibata <thiago.shibatta@gmail.com>
2024-05-27 16:14:43 -0300

    feat: create page new

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles"
+
+import { Header } from "../../components/Header"
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <h1>Criar nota</h1>
+    </Container>
+  )
+}
