b2e51deb949264c5e5ae6ad0220c50c65784ddea
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-11 11:57:02 -0300

    feat: finalized user registration on SignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react"
+
 import { Container, Form, Background } from "./styles"
 import { FiMail, FiLock, FiUser } from "react-icons/fi"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
+import { api } from "../../services/api"
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
 export function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos")
+    }
+    console.log(name, email, password)
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+        navigate("/")
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert("Não foi possível cadastrar!")
+        }
+      })
+  }
   return (
     <Container>
       <Background />
@@ -13,10 +42,25 @@ export function SignUp() {
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
         <h2>Crie sua conta</h2>
-        <Input icon={FiUser} type="text" placeholder="Nome" />
-        <Input icon={FiMail} type="text" placeholder="E-mail" />
-        <Input icon={FiLock} type="password" placeholder="Senha" />
-        <Button title="Cadastrar" />
+        <Input
+          icon={FiUser}
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          icon={FiMail}
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button title="Cadastrar" onClick={handleSignUp} />
         <Link to="/">Voltar para login</Link>
       </Form>
     </Container>
