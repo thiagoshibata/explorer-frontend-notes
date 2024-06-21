9ba1d024dcbedceea99e28d3b64f701925308a8b
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-21 09:20:45 -0300

    feat: add function handleSignOut

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,25 @@
+import { useNavigate } from "react-router-dom"
+import { RiShutDownLine } from "react-icons/ri"
+
 import { useAuth } from "../../hooks/auth"
 import { api } from "../../services/api"
+
 import avatarPlaceHolder from "../../assets/avatar_placeholder.svg"
 
-import { RiShutDownLine } from "react-icons/ri"
 import { Container, Profile, Logout } from "./styles"
 
 export function Header() {
   const { signOut, user } = useAuth()
+  const navigate = useNavigate()
 
   const avatarUrl = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
     : avatarPlaceHolder
+
+  function handleSignOut() {
+    navigate("/")
+    signOut()
+  }
   return (
     <Container>
       <Profile to="/profile">
@@ -20,7 +29,7 @@ export function Header() {
           <strong>{user.name}</strong>
         </div>
       </Profile>
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
