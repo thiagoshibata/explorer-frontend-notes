6349faf8798ee2b1def50f3a88a8b3b269e533d0
thiago-shibata <thiago.shibatta@gmail.com>
2024-06-21 08:54:34 -0300

    feat: loading data on page detail and delete note

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -5,7 +5,7 @@ import { Tag } from "../../components/Tag"
 
 export function Note({ data, ...rest }) {
   return (
-    <Container>
+    <Container {...rest}>
       <h1>{data.title}</h1>
       {data.tags && (
         <footer>
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,10 @@
+import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+
 import { Container, Links, Content } from "./style.js"
+
+import { api } from "../../services/api.js"
+
 import { Header } from "../../components/Header"
 import { Button } from "./../../components/Button"
 import { Section } from "./../../components/Section"
@@ -6,42 +12,66 @@ import { Tag } from "./../../components/Tag"
 import { ButtonText } from "./../../components/ButtonText"
 
 export function Details() {
+  const params = useParams()
+  const [data, setData] = useState("")
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate("/")
+  }
+
+  async function handleRemoveNote() {
+    const confirm = window.confirm("Deseja realmente excluir essa nota?")
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate("/")
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
   return (
     <Container>
       <Header />
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
-          <h1>Introdução ao React</h1>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
-            vero provident consectetur, et nisi quidem ea aliquid adipisci
-            blanditiis dolores iusto ex, officia laborum nostrum est. Soluta
-            inventore praesentium eius? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Culpa facere modi eaque tempora omnis tempore?
-            Neque laborum dicta esse rem hic repudiandae quos quidem non? Quia
-            exercitationem fuga placeat hic. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Cumque nulla provident eum nesciunt
-            vel aliquid vitae eos maxime iure facere porro asperiores dolores
-            pariatur nihil, delectus, sit, sint iste ipsam.
-          </p>
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">https://rocketseat.com.br</a>
-              </li>
-              <li>
-                <a href="#">https://rocketseat.com.br</a>
-              </li>
-            </Links>
-          </Section>
-          <Section title="Marcadores">
-            <Tag title="nodejs" />
-            <Tag title="express" />
-          </Section>
-          <Button title="Voltar" />
-        </Content>
-      </main>
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" onClick={handleRemoveNote} />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+
+            {data.links && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
+
+            {data.tags && (
+              <Section title="Marcadores">
+                {data.tags.map((tag) => (
+                  <Tag key={tag.id} title={tag.name} />
+                ))}
+              </Section>
+            )}
+            <Button onClick={handleBack} title="Voltar" />
+          </Content>
+        </main>
+      )}
     </Container>
   )
 }
